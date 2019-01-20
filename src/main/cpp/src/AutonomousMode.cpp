2c592a52916d634ca00d8b6a7b5d1af6e0c6ee8f
Dylan <nalyd.f@icloud.com>
2019-01-20 15:21:35 -0800

    Added more joystick functions.

--- src/main/cpp/src/AutonomousMode.cpp
+++ src/main/cpp/src/AutonomousMode.cpp
@@ -10,6 +10,7 @@ Autonomous::Autonomous(Disabled *disabled, Drive *drive, ADXRS450_Gyro *gyro,
                        ObservableXboxJoystick *codriver)
         : m_driverJoystick(driver)
         , m_operatorJoystick(codriver)
+        , m_drive(drive)
         , m_driveMode(DriveMode::Cheesy)
         , m_endGameSignalSent(false)
         , m_noAuto(new NoAuto())
@@ -65,7 +66,209 @@ void Autonomous::AutonomousPeriodic() {
      * Operator Joystick
      */
 }
+void Autonomous::HandlePoofsJoystick(uint32_t port, uint32_t button,
+                                     bool pressedP) {
+    if (port == DRIVER_JOYSTICK_PORT) {
+        switch (button) {
+            case PoofsJoysticks::LeftTrigger:
+                if (pressedP) {
+                }
+                else {
+                }
+                break;
+            case PoofsJoysticks::RightTrigger:
+                if (pressedP) {
+                }
+                else {
+                }
+                break;
+            case PoofsJoysticks::LeftBumper:
+                if (pressedP) {
+                }
+                else {
+                }
+                break;
+            case PoofsJoysticks::RightBumper:
+                if (pressedP) {
+                }
+                else {
+                }
+                break;
+        }
+    }
+}
 
+void Autonomous::HandleXboxJoystick(uint32_t port, uint32_t button,
+                                    bool pressedP) {
+    if (port == OPERATOR_JOYSTICK_PORT) {
+        switch (button) {
+            case Xbox::BtnY:
+                if (pressedP) {
+                }
+                else {
+                }
+                break;
+            case Xbox::BtnA:
+                if (pressedP) {
+                }
+                else {
+                }
+                break;
+            case Xbox::BtnX:
+                if (pressedP) {
+                }
+                else {
+                }
+                break;
+            case Xbox::BtnB:
+                if (pressedP) {
+                }
+                else {
+                }
+                break;
+            case Xbox::LeftBumper:
+                if (pressedP) {
+                }
+                else {
+                }
+                break;
+            case Xbox::LJoystickBtn:
+                if (pressedP) {
+                }
+                else {
+                }
+                break;
+            case Xbox::RJoystickBtn:
+                if (pressedP) {
+                }
+                else {
+                }
+                break;
+            case Xbox::RightBumper:
+                if (pressedP) {
+                }
+                else {
+                }
+                break;
+            case Xbox::DPadUpVirtBtn:
+                if (pressedP) {
+                }
+                else {
+                }
+                break;
+            case Xbox::DPadDownVirtBtn:
+                if (pressedP) {
+                }
+                else {
+                }
+                break;
+            case Xbox::DPadLeftVirtBtn:
+                if (pressedP) {
+                }
+                else {
+                }
+                break;
+            case Xbox::DPadRightVirtBtn:
+                if (pressedP) {
+                }
+                else {
+                }
+                break;
+            case Xbox::Back:
+                if (pressedP) {
+                }
+                else {
+                }
+                break;
+            case Xbox::Start:
+                if (pressedP) {
+                }
+                else {
+                }
+                break;
+        }
+    }
+}
+
+void Autonomous::HandleDualActionJoystick(uint32_t port, uint32_t button,
+                                          bool pressedP) {
+    switch (button) {
+        case DualAction::BtnA:
+            if (pressedP) {
+            }
+            else {
+            }
+            break;
+        case DualAction::BtnB:
+            if (pressedP) {
+            }
+            else {
+            }
+            break;
+        case DualAction::BtnX:
+            if (pressedP) {
+            }
+            else {
+            }
+            break;
+        case DualAction::BtnY:
+            if (pressedP) {
+            }
+            else {
+            }
+            break;
+        case DualAction::LeftBumper:
+            if (pressedP) {
+            }
+            else {
+            }
+            break;
+        case DualAction::LeftTrigger:
+            if (pressedP) {
+            }
+            else {
+            }
+            break;
+        case DualAction::RightBumper:
+            if (pressedP) {
+            }
+            else {
+            }
+            break;
+        case DualAction::RightTrigger:
+            if (pressedP) {
+            }
+            else {
+            }
+            break;
+        case DualAction::DPadUpVirtBtn:
+            if (pressedP) {
+            }
+            else {
+            }
+            break;
+        case DualAction::DPadDownVirtBtn:
+            if (pressedP) {
+            }
+            else {
+            }
+            break;
+        case DualAction::DPadLeftVirtBtn:
+            if (pressedP) {
+            }
+            else {
+            }
+            break;
+        case DualAction::DPadRightVirtBtn:
+            if (pressedP) {
+            }
+            else {
+            }
+            break;
+        default:
+            break;
+    }
+}
 void Autonomous::AutonomousStop() {
 }
 }
