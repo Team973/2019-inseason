2e2ae6e9bc2a009d60c6daed2bcb083d7a2f817e
John Pywtorak <john.pywtorak@sri.com>
2019-02-06 13:02:01 -0800

    Added doc comments for the GameMode

--- src/main/cpp/src/GameMode.h
+++ src/main/cpp/src/GameMode.h
@@ -1,3 +1,9 @@
+/*
+ * GameMode.h
+ *
+ *  Created on: February 5, 2019
+ *      Authors: John P, Luis, Dillan
+ */
 #pragma once
 
 #include "frc/WPILib.h"
@@ -6,6 +12,10 @@ using namespace frc;
 
 namespace frc973 {
 
+/*
+ * GameMode represents the robots scoring mode. You can't be doing Cargo and
+ * Hatch at the same time, so it is in one or the other for example.
+ */
 enum class GameMode
 {
     Cargo,
