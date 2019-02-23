a88cf49d113e0320a2231833b395ade90849683f
Dylan <nalyd.f@icloud.com>
2019-02-23 13:25:08 -0800

    changed stinger voltage to 1.0

--- src/main/cpp/src/TeleopMode.h
+++ src/main/cpp/src/TeleopMode.h
@@ -87,7 +87,7 @@ public:
      * @param pressedP The button's new status.
      */
     void HandleXboxJoystick(uint32_t port, uint32_t button, bool pressedP);
-    static constexpr double ELEVATOR_STINGER_VOLTAGE_RATIO = 0.9;
+    static constexpr double ELEVATOR_STINGER_VOLTAGE_RATIO = 1.0;
 
     friend class PresetHandlerDispatcher;
 
