c7d057a5c079bb5b89f34b7100613eb09c297fe6
John Pywtorak <jpywtora@gmail.com>
2019-03-11 16:11:04 -0700

    Initial swing at a front cargo hartch auto using limelight with skew Update AutoCommon.h and FrontHatchCargoShip.cpp

--- src/main/cpp/src/auto/AutoCommon.h
+++ src/main/cpp/src/auto/AutoCommon.h
@@ -1,5 +1,6 @@
 #pragma once
 
 namespace frc973 {
-	static constexpr double HAB_TO_FIELD_DISTANCE = 36.0;
+static constexpr double HAB_TO_FIELD_DISTANCE = 36.0;
+static constexpr double HAB_TURN_TOWARD_TARGET = 6.0;
 }
--- src/main/cpp/src/auto/FrontHatchCargoShip.cpp
+++ src/main/cpp/src/auto/FrontHatchCargoShip.cpp
@@ -2,17 +2,11 @@
 #include "src/auto/AutoCommon.h"
 
 using namespace frc;
+// using namespace trajectories;
 
 namespace frc973 {
 
 /* enumerated AutoSteps
-DriveForwardFromCenter,
-DriveForwardOffLeftPlatform,
-DriveForwardOffRightPlatform,
-TurnTowardLeftCargoFront,
-TurnTowardRightCargoFront,
-VisionRightCargoFront,
-VisionLeftCargoFront,
 ScoreRightCargoFront,
 ScoreLeftCargoFront,
 BackupRightCargoFront,
@@ -27,7 +21,62 @@ BackupFromLeftHumanHatch,
 DriveTowardFrontLowerRocket
 */
 
+/* TrajectoryDescription
+double timestep;           **< The timestep. *
+double max_vel;            **< The maximum velocity. *
+double max_accel;          **< The maximum acceleration. *
+double max_jerk;           **< The maximum jerk. *
+double wheelbase_width;    **< The maximum velocity. *
+int length;                **< The length. *
+Segment *left_trajectory;  **< The left trajectory. *
+Segment *right_trajectory; **< The right trajectory. *
+*/
+
 void Autonomous::CargoShipThenRocketAuto() {
+    double initial_dist = HAB_TO_FIELD_DISTANCE;
+    double start_angle = m_drive->GetAngle();
+    // struct TrajectoryDescription trajDesc = { 0.0, 0.0, 0.0, 0.0, 0.0, 0,
+    // nullptr, nullptr };
+
+    switch (m_autoStateStartPosition) {
+        case AutoStateStartPosition::LeftHabLevel2:
+            initial_dist += 12.0;
+            break;
+        case AutoStateStartPosition::CenterHab:
+            initial_dist -= 12.0;
+            break;
+        case AutoStateStartPosition::RightHabLevel2:
+            initial_dist += 12.0;
+            break;
+    }
+
+    m_autoTimer = GetMsecTime();
+    // m_driveMode in Autonomous? may not be needed
+
+    switch (m_autoStep) {
+        case 0:  // DriveForwardFromAny left, center, or rights (off the Hab)
+            m_drive->PIDDrive(initial_dist, 0.0,
+                              DriveBase::RelativeTo::Absolute, 0.0);
+            m_autoStep++;
+            break;
+        case 1:  // TurnTowardCargoFront
+            if (m_drive->OnTarget()) {
+                m_drive->PIDTurn(HAB_TURN_TOWARD_TARGET * m_direction,
+                                 DriveBase::RelativeTo::Now, 0.0);
+                m_autoStep++;
+            }
+
+            break;
+        case 2:  // VisionCargoFrontInit
+            if (m_drive->OnTarget()) {
+                m_autoStep++;
+            }
+
+            break;
+        case 3:  // VisionCargoFront
+            m_drive->LimelightDriveWithSkew();
+            break;
+    }
 }
 
 void Autonomous::TwoCargoShipAuto() {
