e94d5db7b8a59baaa209e5f7faeee549f2e92280
root <root@DESKTOP-JKUB12B.localdomain>
2019-10-29 19:20:58 -0700

    Made StartToCargo Better

--- src/main/cpp/src/auto/StartToCargo.cpp
+++ src/main/cpp/src/auto/StartToCargo.cpp
@@ -12,38 +12,34 @@ void Autonomous::StartToCargo() {
             break;
         case 1:
             if (m_drive->OnTarget()) {
+                if (m_autoStateStartPosition ==
+                    AutoStateStartPosition::LeftHabLevel2) {
+                    m_limelightHatch->SetCameraVisionLeft();
+                    m_drive->LimelightDriveWithSkew();
+                    m_autoStep++;
+                    break;
+                }
+                else if (m_autoStateStartPosition ==
+                         AutoStateStartPosition::RightHabLevel2) {
+                    m_drive->LimelightDriveWithSkew();
+                    m_limelightHatch->SetCameraVisionRight();
+                    m_autoStep++;
+                    break;
+                }
                 m_autoStep++;
             }
             break;
         case 2:
-            if (m_autoStateStartPosition ==
-                AutoStateStartPosition::LeftHabLevel2) {
-                m_limelightHatch->SetCameraVisionLeft();
-                m_drive->LimelightDriveWithSkew();
-                m_autoStep++;
-                break;
-            }
-            else if (m_autoStateStartPosition ==
-                     AutoStateStartPosition::RightHabLevel2) {
-                m_drive->LimelightDriveWithSkew();
-                m_limelightHatch->SetCameraVisionRight();
-                m_autoStep++;
-                break;
-            }
-        case 3:
             if (m_drive->OnTarget()) {
+                m_hatchIntake->Exhaust();
                 m_autoStep++;
             }
             break;
-        case 4:
-            m_hatchIntake->Exhaust();
-            m_autoStep++;
-            break;
-        case 5:
+        case 3:
             m_drive->PIDDrive(m_dir * -20.0, 0.0, Drive::RelativeTo::Now, 0.8);
             m_autoStep++;
             break;
-        case 6:
+        case 4:
             if (m_drive->OnTarget()) {
                 m_hatchIntake->SetIdle();
                 m_autoStep++;
