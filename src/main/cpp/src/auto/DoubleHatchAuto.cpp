48dd99fd189a07e73191a21b9bb48592d2f44110
John Pywtorak <jpywtora@gmail.com>
2019-10-29 17:52:53 -0700

    Last double hatch auto tweak to human player

--- src/main/cpp/src/auto/DoubleHatchAuto.cpp
+++ src/main/cpp/src/auto/DoubleHatchAuto.cpp
@@ -14,46 +14,37 @@ void Autonomous::DoubleHatchAuto() {
             break;
         case 8:
             if (m_drive->OnTarget()) {
+                m_drive->PIDDrive(m_dir * 155.0, 0.0, Drive::RelativeTo::Now, 0.8);
                 m_autoStep++;
             }
             break;
         case 9:
-            m_drive->PIDDrive(m_dir * 155.0, 0.0, Drive::RelativeTo::Now, 0.8);
-            m_autoStep++;
-            break;
-        case 10:
             if (m_drive->OnTarget()) {
                 m_drive->PIDTurn(m_tur * 45.0, Drive::RelativeTo::Now, 0.8);
                 m_autoStep++;
             }
             break;
-        case 11:
+        case 10:
             if (m_drive->OnTarget()) {
                 m_drive->LimelightDriveWithSkew();
-                m_autoStep++;
-            }
-            break;
-        case 12:
-            if (m_drive->OnTarget()) {
                 m_hatchIntake->RunIntake();
-                m_autoTimer = GetMsecTime();
                 m_autoStep++;
             }
             break;
-        case 13:
-            if (GetMsecTime() - m_autoTimer > 250) {
+        case 11:
+            if (m_drive->OnTarget()) {
                 m_hatchIntake->SetIdle();
                 m_drive->PIDDrive(m_dir * -80.0, 0.0, Drive::RelativeTo::Now, 0.8);
                 m_autoStep++;
             }
             break;
-        case 14:
+        case 12:
             if (m_drive->OnTarget()) {
                 m_drive->PIDTurn(m_tur * 180.0, Drive::RelativeTo::Now, 0.8);
                 m_autoStep++;
             }
             break;
-        case 15:
+        case 13:
             if (m_drive->OnTarget()) {
                 // m_autoStep++;
             }
